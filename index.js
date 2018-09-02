c0eaab26bc59988df44721c26becdd61c61a4e2c
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-09-02 23:44:07 +0530

    getting response in console log

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var request = require('request');
-var express = require('express');
+const request = require('request');
+const express = require('express');
 
 var app =  express();
 
 app.get('/',function (req,res) {
-    res.send('Test Successfull');
+    // res.send('Test Successfull');
+    request('http://api.openweathermap.org/data/2.5/weather?id=1273874&units=metric&appid=2e0ccb64d6edabd98dd5cf754ab2f55f', function (error,response,body) {
+        if(error){
+           return  console.log(error);
+        }
+
+        // res.send(body.coord);
+        return console.log(body);
+
+    })
 });
 
 app.listen(8000);
