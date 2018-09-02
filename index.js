0a110560b73f4904ffa49cd15f0686d76670651e
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-09-03 00:01:38 +0530

    json parsing working

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const express = require('express');
 var app =  express();
 
 app.get('/',function (req,res) {
-    // res.send('Test Successfull');
     request('http://api.openweathermap.org/data/2.5/weather?id=1273874&units=metric&appid=2e0ccb64d6edabd98dd5cf754ab2f55f', function (error,response,body) {
         if(error){
            return  console.log(error);
         }
 
         // res.send(body.coord);
-        return console.log(body);
+        // return console.log(body);
+
+        var data = body;
+        obj = JSON.parse(data);
+
+        return console.log(obj.main.temp);
 
     })
 });
