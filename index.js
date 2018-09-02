0f9dcb37b3b31f186d1e8b9bf6089d27a736564d
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-09-02 23:24:15 +0530

    added request and express

---
+++ index.js
@@ -0,0 +1,10 @@
+var request = require(request);
+var express = require(express);
+
+var app =  express();
+
+app.get('/',function (req,res) {
+    res.send('Test Successfull');
+})
+
+app.listen(8000);
